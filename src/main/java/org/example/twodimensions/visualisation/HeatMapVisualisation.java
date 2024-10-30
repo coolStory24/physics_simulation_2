0b65fb722bd7a9e785ea4d75c9071da36d7aad92
coolstory <mihail.furm@gmail.com>
2024-10-31 01:58:16 +0300

    draw HeatMapVisualisation from file

--- src/main/java/org/example/twodimensions/visualisation/HeatMapVisualisation.java
+++ src/main/java/org/example/twodimensions/visualisation/HeatMapVisualisation.java
@@ -5,13 +5,18 @@ import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.concurrent.atomic.AtomicInteger;
 import javax.swing.*;
 
-public class HeatMapVisualisation extends JPanel implements ActionListener {
+public class HeatMapVisualisation extends JPanel implements ActionListener, KeyListener {
 
-  private int currFramesPerSecond = 20;
+  private AtomicInteger currFramesPerSecond = new AtomicInteger(25);
   private final double[][][] DATA;
+  private Timer timer;
+
+  private boolean isRunning = false;
 
   private double minValue = Double.POSITIVE_INFINITY;
   private double maxValue = Double.NEGATIVE_INFINITY;
@@ -24,13 +29,17 @@ public class HeatMapVisualisation extends JPanel implements ActionListener {
   private final int WIDTH;
   private final int STEPS;
 
+  private final double SECONDS_BETWEEN_STEPS;
+
   private final Font TITLE_FONT = new Font("Serif", Font.BOLD, 24);
   private final Font STANDARD_FONT = new Font("Serif", Font.PLAIN, 18);
   private final int LINE_HEIGHT = 24;
 
   private final AtomicInteger currentFrame = new AtomicInteger(0);
 
-  public HeatMapVisualisation(double[][][] data, int width, int height, int padding) {
+  public HeatMapVisualisation(double secondsBetweenSteps, double[][][] data, int width, int height,
+      int padding) {
+    this.SECONDS_BETWEEN_STEPS = secondsBetweenSteps;
     this.DATA = data;
     this.FIELD_WIDTH = width;
     this.FIELD_HEIGHT = height;
@@ -51,23 +60,35 @@ public class HeatMapVisualisation extends JPanel implements ActionListener {
 
     setFocusable(true);
 
-    Timer timer = new Timer(1000 / currFramesPerSecond, this);
+    addKeyListener(this);
+    timer = new Timer(1000 / currFramesPerSecond.intValue(), this);
     timer.start();
+    isRunning = true;
   }
 
   @Override
   protected void paintComponent(Graphics g) {
     super.paintComponent(g);
     paintBorders(g);
+    int currFrame = currentFrame.get();
+    if (currFrame < STEPS) {
+      paintMap(DATA[currFrame], g);
+    } else {
+      isRunning = false;
+      currentFrame.set(STEPS - 1);
+    }
+
     paintInfo(g);
   }
 
   private void paintBorders(Graphics g) {
     g.setColor(Color.BLACK);
-    g.drawRect(
-        PADDING - 1, PADDING - 1, (FIELD_HEIGHT - PADDING * 2) + 1,
+    g.drawRect(PADDING - 1, PADDING - 1, (FIELD_HEIGHT - PADDING * 2) + 1,
         (FIELD_HEIGHT - PADDING * 2) + 1);
-    paintMap(DATA[currentFrame.getAndIncrement()], g);
+
+    if (isRunning) {
+      currentFrame.getAndIncrement();
+    }
   }
 
   private void paintInfo(Graphics g) {
@@ -75,8 +96,27 @@ public class HeatMapVisualisation extends JPanel implements ActionListener {
     g.setFont(TITLE_FONT);
     g.drawString("Current simulation params", FIELD_HEIGHT, PADDING);
     g.setFont(STANDARD_FONT);
-    g.drawString(String.format("FPS: %d", currFramesPerSecond), FIELD_HEIGHT,
+    g.drawString(String.format("FPS: %d", currFramesPerSecond.intValue()), FIELD_HEIGHT,
         PADDING + LINE_HEIGHT);
+
+    String runningMessage;
+
+    if (currentFrame.intValue() < STEPS - 1) {
+      runningMessage = isRunning ? "Running..." : "Paused, press [SPACE] to continue";
+    } else {
+      runningMessage = "Finished";
+    }
+
+    g.drawString(runningMessage, FIELD_HEIGHT, PADDING + LINE_HEIGHT * 2);
+    g.drawRect(FIELD_HEIGHT, PADDING + LINE_HEIGHT * 3, 200, 10);
+    g.setColor(Color.BLUE);
+    g.fillRect(FIELD_HEIGHT, PADDING + LINE_HEIGHT * 3,
+        (200 * currentFrame.intValue()) / (STEPS - 1), 10);
+
+    g.setColor(Color.BLACK);
+    g.drawString(String.format("Time scale: %.3f",
+            SECONDS_BETWEEN_STEPS * currFramesPerSecond.doubleValue()), FIELD_HEIGHT,
+        PADDING + LINE_HEIGHT * 5);
   }
 
   private void paintMap(double[][] heatMap, Graphics g) {
@@ -87,18 +127,40 @@ public class HeatMapVisualisation extends JPanel implements ActionListener {
         var color = HeatMapColor.getHeatMapColor(heatMap[x][y], minValue, maxValue);
         g.setColor(color);
 
-        g.fillRect(CELL_SIZE * x + PADDING, CELL_SIZE * y + PADDING, CELL_SIZE,
-            CELL_SIZE);
+        g.fillRect(CELL_SIZE * x + PADDING, CELL_SIZE * y + PADDING, CELL_SIZE, CELL_SIZE);
       }
     }
   }
 
   @Override
   public void actionPerformed(ActionEvent e) {
-
-    if (currentFrame.intValue() >= DATA.length - 1) {
-      ((Timer) e.getSource()).stop();
-    }
     repaint();
+  }
+
+  @Override
+  public void keyTyped(KeyEvent e) {
+  }
+
+  @Override
+  public void keyPressed(KeyEvent e) {
+    switch (e.getKeyCode()) {
+      case KeyEvent.VK_SPACE -> isRunning = !isRunning;
+      case KeyEvent.VK_RIGHT ->
+          currentFrame.set(Math.min(currentFrame.intValue() + STEPS / 20, STEPS - 1));
+      case KeyEvent.VK_LEFT -> currentFrame.set(Math.max(currentFrame.intValue() - STEPS / 20, 0));
+      case KeyEvent.VK_UP -> {
+        timer.setDelay(1_000 / currFramesPerSecond.incrementAndGet());
+      }
+      case KeyEvent.VK_DOWN -> {
+        int val = currFramesPerSecond.get();
+        if (val > 1) {
+          timer.setDelay(1_000 / currFramesPerSecond.compareAndExchange(val, val - 1));
+        }
+      }
+    }
+  }
+
+  @Override
+  public void keyReleased(KeyEvent e) {
   }
 }
--- src/main/java/org/example/twodimensions/visualisation/VisualisationFrame.java
+++ src/main/java/org/example/twodimensions/visualisation/VisualisationFrame.java
@@ -1,6 +1,13 @@
 package org.example.twodimensions.visualisation;
 
 import java.awt.HeadlessException;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
 import javax.swing.JFrame;
 
 public class VisualisationFrame extends JFrame {
@@ -11,7 +18,33 @@ public class VisualisationFrame extends JFrame {
 
   public VisualisationFrame(double[][][] data) throws HeadlessException {
     setBounds(0, 0, FIELD_WIDTH, FIELD_HEIGHT);
-    add(new HeatMapVisualisation(data, FIELD_WIDTH, FIELD_HEIGHT, PADDING));
+    add(new HeatMapVisualisation(0.1, data, FIELD_WIDTH, FIELD_HEIGHT, PADDING));
     setVisible(true);
+  }
+
+  public VisualisationFrame(String filePath) throws HeadlessException, FileNotFoundException {
+    try (Scanner scanner =
+        new Scanner(new FileInputStream(filePath)).useLocale(
+            Locale.ENGLISH)) {
+      double secondsBetweenSteps = scanner.nextDouble();
+      int size = scanner.nextInt();
+      int frames = scanner.nextInt();
+      var data = new double[frames][size][size];
+
+      for (int fr = 0; fr < frames; fr++) {
+        for (int i = 0; i < size; i++) {
+          for (int j = 0; j < size; j++) {
+            int frame = scanner.nextInt();
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            data[frame][x][y] = scanner.nextDouble();
+          }
+        }
+      }
+
+      setBounds(0, 0, FIELD_WIDTH, FIELD_HEIGHT);
+      add(new HeatMapVisualisation(secondsBetweenSteps, data, FIELD_WIDTH, FIELD_HEIGHT, PADDING));
+      setVisible(true);
+    }
   }
 }
